2e0efe13fd4b2bef91b19d5c948449d359532806
vishak-32 <spvishak8@gmail.com>
2024-08-09 23:25:34 +0530

    Update RegistrationF.js

--- RegistrationF.js
+++ RegistrationF.js
@@ -11,18 +11,26 @@ import React from "react";
     return(
         <>
         <center>
+     
         <h1>Registration Form</h1><br></br>
+     
         <label>Name</label><br></br>
         <input type="text" id="name" name="name"></input><br></br><br></br>
+     
         <label>age</label><br></br>
         <input type="number" id="age" name="age"></input><br></br><br></br>
+     
         <label>phone number</label><br></br>
         <input type="tel" id="num" name="num"></input><br></br><br></br>
+     
         <label>Email address</label><br></br>
         <input type="email" id="email" name="email"></input><br></br><br></br>
+     
         <label>Date of birth</label><br></br>
         <input type="date" id="dob" name="dob"></input><br></br><br></br> 
+     
         <input type="button" value="submit" onClick={form}></input>
+     
         </center>
         </>
     )
