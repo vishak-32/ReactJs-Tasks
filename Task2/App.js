2c9cfdc5587fab7c1eecbe6c35519cdc2a6cc0cc
vishak-32 <spvishak8@gmail.com>
2024-08-10 22:33:13 -0700

    Add files via upload

---
+++ Task2/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Task2 from './Compounds/Task2';
+import './Compounds/Style.css';
+
+function App() {
+  return (
+<>
+<Task2/>
+</>
+  );
+}
+
+export default App;
---
+++ Task2/Task2.js
@@ -0,0 +1,19 @@
+import React from "react";
+const Task2 =() => {
+  return(
+  
+      <>
+    
+    <div className="Container">
+      <div className="insideCard">
+          <h1>INDIAN SUPER LEAGUE</h1>
+        <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" className="RCB"/>
+        <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" className="CSK"/>
+      </div>
+    </div>
+  
+      </>
+  )
+  
+  }
+export default Task2;
