d323368375b14c1e4b025fef93d4c2f0c315187a
vishak-32 <spvishak8@gmail.com>
2024-08-10 22:58:35 -0700

    Add files via upload

---
+++ Task 3/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Task3 from './Compounds/Task3';
+import './Compounds/Style.css';
+
+function App() {
+  return (
+<>
+<Task3/>
+</>
+  );
+}
+
+export default App;
---
+++ Task 3/Task3.js
@@ -0,0 +1,20 @@
+import React from "react";
+const Task3 =() => {
+  return(
+  
+      <>
+        <div className="button">
+            <h1>Buttons</h1>
+            <button type="button"  className="btn btn-primary-m5">Like</button>
+            <button type="button" className="btn1 btn-success-m5">Command</button>
+            <button type="button" className="btn2 btn-warning-m5">Share</button>
+            </div>
+            
+    
+
+  
+      </>
+  )
+  
+  }
+export default Task3;
