89b0b7d9edb3889440327f578dec79ae12a94027
vishak-32 <spvishak8@gmail.com>
2024-08-10 22:13:33 -0700

    Add files via upload

---
+++ Task 1/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Home from './Compounds/Home';
+
+function App() {
+  return (
+<>
+<Home/>
+</>
+  );
+}
+
+export default App;
---
+++ Task 1/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+const Home =() => {
+  return (
+    <>
+  <div className="Container">
+    <h1 className="Main">Congratulations</h1>
+    <div className="insideCard">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h2 className="skywalker">Vishak</h2>
+      <p>KG College of Arts and Science</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+  </>
+  )
+};
+
+export default Home;
