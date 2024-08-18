be9ba573708f382caefd3221c71393adc9d37e9c
vishak-32 <spvishak8@gmail.com>
2024-08-18 19:08:21 +0530

    Add files via upload

---
+++ Router Task-1/src/compoent/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react"
+import "./navbar.css"
+import { Link } from "react-router-dom"
+import { FaBars } from "react-icons/fa"
+import { ImCross } from "react-icons/im"
+
+const Navbar = () => {
+  const [Mobile, setMobile] = useState(false)
+  return (
+    <>
+      <nav className='navbar'>
+        <h3 className='logo'>Logo</h3>
+        <ul className={Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}>
+          <Link to='/' className='home'>
+            <li>Home</li>
+          </Link>
+          <Link to='/about' className='about'>
+            <li>About</li>
+          </Link>
+          <Link to='/services' className='services'>
+            <li>Services</li>
+          </Link>
+          <Link to='/skills' className='skills'>
+            <li>Skills</li>
+          </Link>
+          <Link to='/contact' className='home'>
+            <li>contact</li>
+          </Link>
+        </ul>
+        <button className='mobile-menu-icon' onClick={() => setMobile(!Mobile)}>
+          {Mobile ? <ImCross /> : <FaBars />}
+        </button>
+      </nav>
+    </>
+  )
+}
+export default Navbar
